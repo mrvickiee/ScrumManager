7d582f8550b16e784b044a4923ec3a841660aa4e
BennyKJohnson <bkj986@uowmail.edu.au>
2016-03-16 13:23:01 +1100

    Initial Commit

---
+++ ScrumManager/ScrumManager.h
@@ -0,0 +1,19 @@
+//
+//  ScrumManager.h
+//  ScrumManager
+//
+//  Created by Ben Johnson on 16/03/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ScrumManager.
+FOUNDATION_EXPORT double ScrumManagerVersionNumber;
+
+//! Project version string for ScrumManager.
+FOUNDATION_EXPORT const unsigned char ScrumManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrumManager/PublicHeader.h>
+
+
