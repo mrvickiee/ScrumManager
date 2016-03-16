d708a1804954d46e54e77d9b95fda4ae1a7d1cec
BennyKJohnson <bkj986@uowmail.edu.au>
2016-03-16 13:49:08 +1100

    Setup project and added Perfect Handlers

---
+++ ScrumManager/PerfectHandlers.swift
@@ -0,0 +1,29 @@
+//
+//  PerfectHandlers.swift
+//  SwiftBlog
+//
+//  Created by Benjamin Johnson on 9/02/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+import PerfectLib
+import MongoDB
+
+// This is the function which all Perfect Server modules must expose.
+// The system will load the module and call this function.
+// In here, register any handlers or perform any one-time tasks.
+
+public func PerfectServerModuleInit() {
+    
+    // Register our handler class with the PageHandlerRegistry.
+    // The name "TTHandler", which we supply here, is used within a mustache template to associate the template with the handler.
+    
+    // Do routing
+    Routing.Handler.registerGlobally()
+  
+    Routing.Routes["GET", "/"] = { _  in StaticFileHandler() }
+
+    print("\(Routing.Routes.description)")
+    
+    
+}
